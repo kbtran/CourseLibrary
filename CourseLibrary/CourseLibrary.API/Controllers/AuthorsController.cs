aeeb02eb220d45742442475bdec7890ae82e3c43
Bruce Tran <bruce.tran@gmail.com>
2020-06-07 22:51:38 -0400

    Add AuthorsController

---
+++ CourseLibrary/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,30 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CourseLibrary.API.Controllers
+{
+    // Could have use : Controller, but since this is an API, dont need to support View
+
+    [ApiController]
+    [Route("api/authors")]
+    // or [Route("api/controller")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibrayRepository;
+        
+        public AuthorsController(ICourseLibraryRepository courseLibrayRepository)
+        {
+            _courseLibrayRepository = courseLibrayRepository ??
+                throw new ArgumentNullException(nameof(courseLibrayRepository));
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibrayRepository.GetAuthors();
+
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}

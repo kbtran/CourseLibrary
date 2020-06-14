16e6e756e463146f7377e8d08375f964117f0c9a
Bruce Tran <bruce.tran@gmail.com>
2020-06-13 20:20:50 -0400

    Add exception handler for production

--- CourseLibrary/CourseLibrary.API/Startup.cs
+++ CourseLibrary/CourseLibrary.API/Startup.cs
@@ -7,6 +7,7 @@ using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -54,6 +55,18 @@ namespace CourseLibrary.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+                    });
+                });
+
             }
 
             app.UseHttpsRedirection();

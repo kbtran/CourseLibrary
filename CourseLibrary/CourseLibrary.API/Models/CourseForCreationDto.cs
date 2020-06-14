e87a94dad4e7fd97fd54c7a5c2245636b0593235
Bruce Tran <bruce.tran@gmail.com>
2020-06-14 00:54:30 -0400

    Add custom validation attribute -CourseTitleMustBeDifferentFromDescription and use it the CourseForCreationDto

--- CourseLibrary/CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary/CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto : IValidatableObject
+    [CourseTitleMustBeDifferentFromDescription]
+    public class CourseForCreationDto // : IValidatableObject
     {
         [Required]
         [MaxLength(100)]
@@ -12,14 +14,14 @@ namespace CourseLibrary.API.Models
         [MaxLength(1500)]
         public string Description { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if(Title == Description )
-            {
-                yield return new ValidationResult(
-                    "The provided description should be different from the title.",
-                    new[] { "CourseForCreationDto" });
-            }
-        }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if(Title == Description )
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided description should be different from the title.",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
---
+++ CourseLibrary/CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -0,0 +1,22 @@
+﻿using CourseLibrary.API.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.ValidationAttributes
+{
+    public class CourseTitleMustBeDifferentFromDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var course = (CourseForCreationDto)validationContext.ObjectInstance;
+
+            if (course.Title == course.Description)
+            {
+                return new ValidationResult(
+                    "The provided description should be different from the title.",
+                    new[] { nameof(CourseForCreationDto) });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}

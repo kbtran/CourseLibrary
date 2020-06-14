125bff1261868baf290b2666e9d8c1b95d341834
Bruce Tran <bruce.tran@gmail.com>
2020-06-14 13:57:22 -0400

    Add validation

--- CourseLibrary/CourseLibrary.API/Models/CourseForUpdateDto.cs
+++ CourseLibrary/CourseLibrary.API/Models/CourseForUpdateDto.cs
@@ -1,8 +1,17 @@
-﻿namespace CourseLibrary.API.Models
+﻿using CourseLibrary.API.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.Models
 {
+    [CourseTitleMustBeDifferentFromDescription(ErrorMessage = "Title must be different from description.")]
     public class CourseForUpdateDto
     {
+        [Required(ErrorMessage = "You should fill out a title.")]
+        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "You should fill out a description.")]
+        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
         public string Description { get; set; }
     }
 }

06fe071ff3ced524b6c50f9d5c06369bd39b1e46
Bruce Tran <bruce.tran@gmail.com>
2020-06-13 23:28:19 -0400

    Add GetAuthorsOption

--- CourseLibrary/CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -90,5 +90,12 @@ namespace CourseLibrary.API.Controllers
 
             return CreatedAtRoute("GetAuthor",  new { authorId = authorToReturn.Id }, authorToReturn);
         }
+
+        [HttpOptions]
+        public IActionResult GetAuthorsOptions()
+        {
+            Response.Headers.Add("Allow", "GET,OPTIONS,POST");
+            return Ok();
+        }
     }
 }

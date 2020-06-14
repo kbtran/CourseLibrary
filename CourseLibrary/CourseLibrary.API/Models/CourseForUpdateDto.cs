485b1cbe04c6e9e8f280fbf44840a4ab161e9d3b
Bruce Tran <bruce.tran@gmail.com>
2020-06-14 14:11:01 -0400

    Refactor code to remove duplication by using the new CourseForManipulationDto

--- CourseLibrary/CourseLibrary.API/Models/CourseForUpdateDto.cs
+++ CourseLibrary/CourseLibrary.API/Models/CourseForUpdateDto.cs
@@ -3,15 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    [CourseTitleMustBeDifferentFromDescription(ErrorMessage = "Title must be different from description.")]
-    public class CourseForUpdateDto
+    public class CourseForUpdateDto : CourseForManipulationDto
     {
-        [Required(ErrorMessage = "You should fill out a title.")]
-        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
-        public string Title { get; set; }
-
         [Required(ErrorMessage = "You should fill out a description.")]
-        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
-        public string Description { get; set; }
+        public override string Description { get => base.Description; set => base.Description = value; }
     }
 }
